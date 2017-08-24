6e294414d05192e20f945be255832f5b25f4ae80
killruana <killruana@gmail.com>
2017-08-24 22:02:08 +0200

    Code clean up. Add documentation and comments.

--- laura/__init__.py
+++ laura/__init__.py
@@ -4,53 +4,93 @@ import logging
 import re
 import requests
 
+# Regex for matching HTTP(S) URLs
+REGEX_HTTP_URL = re.compile("https?://\S+")
 
-REGEX_URL = re.compile("https?://\S+")
 
-
-def extract_url(message):
-    result = REGEX_URL.search(message)
+def extract_http_url(str_):
+    """Extract a HTTP(S) URL from a string
+    :param str_: the string
+    :return: the first HTTP(s) URL found or None otherwise
+    """
+    # Use a regex for searching a URL
+    result = REGEX_HTTP_URL.search(str_)
     if result is None:
-        raise ValueError
+        # No URL found, return None
+        return None
 
+    # Get the URL from the match result
     url = result.group(0)
 
     return url
 
 
 class Laura(fredirc.BaseIRCHandler):
+    """Main class"""
     def __init__(self, channel):
+        """Constructor
+        :param channel: channel to join when connection is made
+        """
         self.channel = channel
 
-    def handle_register(self):
-        self.client.join(self.channel)
-
     def handle_channel_message(self, channel, message, sender):
-        try:
-            url = extract_url(message)
-        except ValueError:
+        """Received a message to a channel.
+        :param channel: the channel name
+        :param message: the received message
+        :param sender: sender of the message
+        """
+        # Search an HTTP(S) URL in the message
+        url = extract_http_url(message)
+        if url is None:
+            # URL not found, abort
             return
 
+        # Get the resource
         r = requests.get(url)
         if r.status_code != 200:
+            # Cannot get the resource, abort
             return
 
+        # Checks if the ressource is an HTML docu;ent
         if not r.headers['content-type'].startswith('text/html'):
+            # Not the good type, abort
             return
 
+        # Parse the HTML document and get the title
         soup = bs4.BeautifulSoup(r.text, 'html.parser')
         title = soup.title.string
 
+        # Clean up the title
+        title = title.strip()
+
+        # Check if the title is not empty
+        # This test must be done after cleanup because the cleanup
+        # step can generate an empty title from a non-empty title
+        if not title:
+            # The title is empty, abort
+            return
+
+        # Send the title in the channel
         self.client.send_message(channel, title)
+
+    def handle_register(self):
+        """The client successfully registered to the server."""
+        # Join the specified channel
+        self.client.join(self.channel)
 
 
 def main():
+    """Main function"""
+
+    # Configuration
     irc_server = "irc.zcraft.fr"
     irc_nick = "laura"
     irc_channel = "#sandbot"
     irc_channel = "#kamoulox"
 
-    client = fredirc.IRCClient(Laura(irc_channel), irc_nick, irc_server)
+    # Create a Laura instance, a client and start the client
+    laura = Laura(irc_channel)
+    client = fredirc.IRCClient(laura, irc_nick, irc_server)
     client.run()
 
 
